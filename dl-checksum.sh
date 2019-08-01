258a614de9cd3acb452c113ee28624cdb7ef7fb5
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-08-01 16:11:39 -0500

    simplify

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -3,6 +3,6 @@ VER=7.3.0
 DIR=~/Downloads
 APP=elasticsearch
 PLATFORM=linux-x86_64
-URL=https://artifacts.elastic.co/downloads/elasticsearch/${APP}-${VER}-${PLATFORM}.tar.gz.sha512
+URL=https://artifacts.elastic.co/downloads/${APP}/${APP}-${VER}-${PLATFORM}.tar.gz.sha512
 printf "  # %s\n" $URL
 printf "  '%s': sha512:%s\n" $VER $(curl -sSL $URL | awk '{print $1}')
